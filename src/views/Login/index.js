469038340b1131f7d241be4303ae931ad69f0120
janielcarneiro <janielcl16@gmail.com>
2022-11-20 12:54:34 -0300

    tela de login feito, a entrada de dados. agora programar o Compomente botão

--- src/views/Login/index.js
+++ src/views/Login/index.js
@@ -1,13 +1,49 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { 
+    Text, 
+    View, 
+    Image, 
+    SafeAreaView, 
+    TextInput, 
+    Button } 
+    from "react-native";
 
 import styles from './styles';
+
+import logo from '../../assets/Revenue-bro.png';
 
 export default function Login(){
 
     return(
+        
         <View style={styles.container}>
-            <Text>Olá estou no login</Text>
+            <View style={styles.caixa}>
+
+                <View style={styles.marca}>
+                    <Image source={logo} style={styles.logo}/>
+                    <Text style={styles.login}>Login</Text>
+                </View>
+
+                <View style={styles.CaixaInput}>
+                    <SafeAreaView>
+
+                        <TextInput 
+                            style={styles.input} 
+                            placeholder="Nome Usuario" 
+                            keyboardType="text" 
+                        />
+
+                        <TextInput 
+                            style={styles.input} 
+                            placeholder="Senha" 
+                            keyboardType="text" 
+                        />
+
+                    </SafeAreaView>
+                </View>
+
+            </View>
+            
         </View>
     )
 }
--- src/views/Login/styles.js
+++ src/views/Login/styles.js
@@ -3,10 +3,67 @@ import { StyleSheet } from "react-native";
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        width: '100%',
+        height: '100%',
+        backgroundColor: '#151515',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+
+    caixa: {
+        width: '90%',
+        height: '90%',
         backgroundColor: '#fff',
         alignItems: 'center',
+        justifyContent: 'flex-start',
+        
+    },
+
+    marca: {
+        width: 150,
+        height: 150,
+        alignItems: 'center',
         justifyContent: 'center',
+    },
+
+    logo: {
+        width: '100%',
+        height: '100%',
+        marginTop: 8,
+    },
+    login:{
+        fontSize: 30,
+        color: "green",
+    },
+
+    CaixaInput: {
+        width: '100%',
+        height: 200,
+        backgroundColor: 'green',
+        marginTop: 25,
+    },
+
+    input: {
+        height: 60,
+        margin: 12,
+        borderWidth: 1,
+        borderColor: '#FAFAFA',
+        borderRadius: 5,
+        padding: 10,
+        fontSize: 17,
+    },
+
+    Caixabotao:{
+        width: '100%',
+        height: 80,
+        backgroundColor: 'pink',
+    },
+
+    botao:{
+        width:100,
+        
     }
+    
 })
 
 export default styles;
